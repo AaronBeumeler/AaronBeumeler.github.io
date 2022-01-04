68ac5db5f96f2fb870a98e903488f7066fd97e40
AaronBeumeler <76123248+AaronBeumeler@users.noreply.github.com>
2022-01-04 13:43:31 -0600

    Update script.js

--- mj_mobile/script.js
+++ mj_mobile/script.js
@@ -11,3 +11,73 @@ function layoutHandler() {
 
 window.onresize = layoutHandler;
 layoutHandler();
+
+function hideStartButton() {
+	var x = document.getElementById("start");
+	x.style.display = "none";
+}
+
+function showTopLeftGameplay() {
+	var x = document.getElementById("topLeftGameplay");
+	x.style.display = "block";
+}	
+
+function updateList() {
+	confirmList.unshift(numberInput + " x " + bNum + " = " + guess + "<br>");
+	document.getElementById("confirmMain").innerHTML = confirmList.join(" ");
+}
+
+function check() {
+	correct = (slotVal * bNum);
+	guess = document.getElementById("guess").value;
+	result = document.getElementById("result").innerHTML;
+	if (guess == null) {
+		wrong += 1;
+		totalCount += 1;
+	}
+	if (guess == correct) {
+		updateList();
+		document.getElementById("result").style.color = "rgb(141, 234, 73)";
+		count += 1;
+		gemTotal += 1;
+		totalCount += 1;
+		playChime();
+		bNum = Math.floor(Math.random() * 13);
+		document.getElementById("random").innerHTML = "x " + bNum;
+		document.getElementById("count").innerHTML = count;
+		document.getElementById("guess").focus();
+	} else if (guess != correct) {
+		playTrumpet();
+		updateWrong();
+		totalCount += 1;
+		wrong += 1;
+		document.getElementById("result").style.color = "red";
+		bNum = Math.floor(Math.random() * 13);
+		document.getElementById("random").innerHTML = "x " + bNum;
+		document.getElementById("guess").focus();
+		clearTimeout(oneMin);
+	}
+    if (totalCount == 20) {
+		stopRoundMusic();
+		clearTimeout(timer);
+		document.getElementById("count").innerHTML = count;
+		document.getElementById("guess").focus();
+		document.getElementById("clock").innerHTML = "End";
+		hideCenterCluster();
+		hideEnterButton();
+		showReset();
+		showAnswerMain();
+		finishTime = timeLeft;
+		showStats();
+		showStatsHead();
+		if (count == 20) {
+			delayMenuMusic();
+			playWinnerMusic();
+		} else {
+			delayMenuMusic();
+			playTubaFall();
+		}
+    }
+    inputClear();
+	lastBottomNum = bNum;
+}
