4f6a2c4ef3734765cd5412a3ce0f7e252ea8b44d
AaronBeumeler <awbeumeler@ualr.edu>
2022-01-28 15:20:17 +0000

    mj-mobile

--- mj_mobile/script.js
+++ mj_mobile/script.js
@@ -4,7 +4,7 @@ function layoutHandler() {
         styleLink.setAttribute("href", "m_style.css");
     } /*else if (window.innerWidth < 1200) {
         styleLink.setAttribute("href", "assets/style/medium.css");
-    } */else if (window.innerWidth >= 700) {
+    } */else /*if (window.innerWidth >= 700)*/ {
         styleLink.setAttribute("href", "style.css");
     }
 }
