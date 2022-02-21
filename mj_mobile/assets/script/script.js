5d93f52faf2c6751dcb1eaeae012e343f7c644dd
AaronBeumeler <awbeumeler@ualr.edu>
2022-02-21 20:25:47 +0000

    testing using only m_style.css

--- mj_mobile/assets/script/script.js
+++ mj_mobile/assets/script/script.js
@@ -1,13 +1,11 @@
+/*
 function layoutHandler() {
     var styleLink = document.getElementById("pagestyle");
     if (window.innerWidth < 700) {
         styleLink.setAttribute("href", "assets/css/m_style.css");
-    } /*else if (window.innerWidth < 1200) {
-        styleLink.setAttribute("href", "assets/style/medium.css");
-    } */else /*if (window.innerWidth >= 700)*/ {
-        styleLink.setAttribute("href", "assets/css/style.css");
     }
 }
+*/
 
 window.onresize = layoutHandler;
 layoutHandler();
